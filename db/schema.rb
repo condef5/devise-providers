4c653b5074a5005d0ae723413c5e72825e8fd480
condef5 <davis.con.fab@gmail.com>
2019-04-17 08:52:04 -0500

    Add table provider

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_205243) do
+ActiveRecord::Schema.define(version: 2019_04_16_214408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2019_04_16_205243) do
     t.integer "progress"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "providers", force: :cascade do |t|
+    t.string "name"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_providers_on_user_id"
   end
 
   create_table "rentals", force: :cascade do |t|
@@ -65,11 +74,10 @@ ActiveRecord::Schema.define(version: 2019_04_16_205243) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "episodes", "series", column: "serie_id"
+  add_foreign_key "providers", "users"
 end

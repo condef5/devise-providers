9c402ba16d7e48daeeff67aaabb335280f7f8ead
condef5 <davis.con.fab@gmail.com>
2019-04-17 08:55:58 -0500

    Add provider

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
     if @user
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: "Facebook")

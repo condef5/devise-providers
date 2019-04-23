e276abf8a75ffea8ad1bccdd1131d8f5a5942cc8
condef5 <davis.con.fab@gmail.com>
2019-04-23 10:02:04 -0500

    Move credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,17 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =
+    {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      user_name: ENV['MAIL_ACCOUNT'],
+      password: ENV['MAIL_PASSWORD'],
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?

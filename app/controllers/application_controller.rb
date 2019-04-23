33c3eb6e21b32da6df466d46c4b1c5b428af865f
condef5 <davis.con.fab@gmail.com>
2019-04-22 23:36:53 -0500

    Install pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   before_action :authenticate_user!, :except => [:show, :index]
+  
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+ 
+  private
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to "/"
+  end
 end

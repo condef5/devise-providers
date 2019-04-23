f0e935cd537a7230dd3a1cb043d8a5fb90552edb
condef5 <davis.con.fab@gmail.com>
2019-04-23 00:19:24 -0500

    Add atributes in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook github twitter google_oauth2 instagram]
+
   def self.from_omniauth(auth)
     user = where(email: auth.info.email).first_or_create do |user|
       user.email = auth.info.email
+      user.role = "regular"
       user.password = Devise.friendly_token[0, 20]
     end
     provider = Provider.find_or_create_by(name: auth.provider, uid: auth.uid, user_id: user.id)
     user
   end
+
+  def admin?
+    self.role == "admin"
+  end
+
+  def regular?
+    self.role == "regular"
+  end
+  
 end
